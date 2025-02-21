137f160245b1b57e03617f9a18f355d3fd0e178a
3dalgolab <threealgo@outlook.com>
2025-02-21 22:46:22 +0900

    Remove ignored files

--- build/lib/panda_test_env/__init__.py
+++
@@ -1,4 +0,0 @@
-from gym.envs.registration import register
-
-__version__ = "0.0.4"
-register(id="panda-test-v0", entry_point="panda_test_env.envs:PandaEnv")
--- build/lib/panda_test_env/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from panda_test_env.envs.panda_env import PandaEnv
